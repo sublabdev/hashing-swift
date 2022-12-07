de215a064483df21edd955c11cef971184f1450d
Tigran Iskandaryan <tiskandaryan@griddynamics.com>
2022-12-07 17:21:56 +0300

    Added documentation comments to XX.swift file

--- HashingSwift/Classes/Hashing/Hashers/XX.swift
+++ HashingSwift/Classes/Hashing/Hashers/XX.swift
@@ -3,6 +3,10 @@ import xxHash_Swift
 import CommonSwift
 
 private extension Data {
+    /// Decodes data using XX hash algorithm
+    /// - Parameters:
+    ///     - width: The expected width of data
+    ///     - Returns: XX-decoded data
     func xx(width: Int64) -> Data {
         (0..<width / 64).compactMap {
             let xxHash64 = XXH64(UInt64($0))
@@ -17,14 +21,20 @@ private extension Data {
 }
 
 extension Hashing {
+    /// Decodes data using XX hash algorithm with width of 64 bytes
+    /// - Returns: XX-decoded data
     public func xx64() -> Data {
         data.xx(width: 64)
     }
     
+    /// Decodes data using XX hash algorithm with width of 128 bytes
+    /// - Returns: XX-decoded data
     public func xx128() -> Data {
         data.xx(width: 128)
     }
     
+    /// Decodes data using XX hash algorithm with width of 256 bytes
+    /// - Returns: XX-decoded data
     public func xx256() -> Data {
         data.xx(width: 256)
     }
